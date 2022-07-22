05b17153cc33b2ef8de9dc8ff080d9dc39107319
Yeonmi <yooyeonmi122@gmail.com>
2022-07-22 19:09:00 +0900

    first commit

---
+++ move.js
@@ -0,0 +1,17 @@
+function move_menu(num){
+    if (num==1){
+        window.location.href="./breed.html"
+    }
+    else if(num==2){
+        window.location.href="./behavior.html"
+    }
+    else if(num==3){
+        window.location.href="./shelter.html"
+    }
+    else if(num==4){
+        window.location.href="./book.html"
+    }
+    else if (num==5){
+        window.location.href="./main.html"
+    }
+}
